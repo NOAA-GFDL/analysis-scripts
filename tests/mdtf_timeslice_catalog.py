e38859e8ed4fecd6dca2f966a6dd2e28122b9fe2
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-01 17:22:54 -0400

    Update mdtf_timeslice_catalog.py

--- tests/mdtf_timeslice_catalog.py
+++ tests/mdtf_timeslice_catalog.py
@@ -4,7 +4,7 @@ from scripts import gen_intake_gfdl
 import sys
 
 input_path = "archive/oar.gfdl.mdtf/MDTF-examples/mdtf-time-slice-example/gfdl.ncrc5-deploy-prod-openmp/pp"
-output_path = "cats/gfdl_analysis_citest"
+output_path = "gfdl_analysis_citest"
 sys.argv = ['INPUT_PATH', input_path, output_path]
 print(sys.argv)
 gen_intake_gfdl.main()
