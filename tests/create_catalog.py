4bdff8f5b8be563043b9d6e98249f95bd7a0a93c
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-07-25 15:40:13 -0400

    Update create_catalog.py

--- tests/create_catalog.py
+++ tests/create_catalog.py
@@ -1,10 +1,11 @@
 from catalogbuilder.scripts import gen_intake_gfdl
 import sys
 
-def create_catalog(yaml, output="data-catalog"):
+def create_catalog(path, yaml, output="data-catalog"):
     """Creates a data catalog from a config file
 
     Args:
+        path: Location of data
         yaml: YAML config file (e.g. mdtf_timeslice_catalog.yaml)
         output: (prefix)Name of the data catalog that will be created.
 
@@ -13,8 +14,8 @@ def create_catalog(yaml, output="data-catalog"):
     """
     yaml_path = Path(__file__).resolve().parent / "mdtf_timeslice_catalog.yaml"
     return Path(f"{output}.json").resolve(), Path(f"{output}.csv").resolve()
-  
-    sys.argv = ['input_path','--config', yaml_path]
+
+    sys.argv = ['str(path)','--config', yaml_path]
     print(sys.argv)
     gen_intake_gfdl.main()
     return Path(f"{output}.json").resolve(), Path(f"{output}.csv").resolve()
