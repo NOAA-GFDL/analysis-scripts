ee5152d3721a1ab8ae45a20737ec57d2f21d9984
Aparna Radhakrishnan <aparna.radhakrishnan@noaa.gov>
2024-09-26 18:45:11 -0400

    Update test_freanalysis_clouds.py

--- tests/test_freanalysis_clouds.py
+++ tests/test_freanalysis_clouds.py
@@ -52,7 +52,7 @@ def test_freanalysis_clouds():
         chdir(Path(tmp))
         path = download_test_data(stem=tmp)
         yaml = Path(__file__).resolve().parent / "mdtf_timeslice_catalog.yaml"
-        outputpath = "/home/runner/work/analysis-scripts-fork/data-catalog"      
+        outputpath = "data-catalog"      
         #Creates data catalog using the scripts in catalogbuilder
         csv, json = gen_intake_gfdl.create_catalog(input_path=str(path),output_path=outputpath,config=str(yaml))
         print(json,csv)
