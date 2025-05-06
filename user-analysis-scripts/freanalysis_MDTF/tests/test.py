afcb46200546cb1330119126dd2969c0f52ce793
Chris Blanton <Chris.Blanton@ldt-4359643.gfdl.noaa.gov>
2025-05-06 17:27:43 -0400

    Change mdtf plugin's git source to a branch on MDTF and updated test script to include all configuration needed and point to a more permanent test input directory.

--- user-analysis-scripts/freanalysis_MDTF/tests/test.py
+++ user-analysis-scripts/freanalysis_MDTF/tests/test.py
@@ -5,12 +5,15 @@ print(available_plugins())
 
 # run the 'example' MDTF POD
 name = "freanalysis_MDTF"  # name of the custom analysis script you want to run
-catalog_path = '/local/home/Jacob.Mims/djptest/mdtf/catalog.json' # absolute path to catalog .json file
-out_dir = '/local/home/Jacob.Mims/analysistest/' # absolute path to dir that you would like the MDTF to run and output
+catalog_path = "/work/c2b/freanalysis_MDTF/catalog.json" # absolute path to catalog .json file
+out_dir = "/work/c2b/freanalysis_MDTF_output" # absolute path to dir that you would like the MDTF to run and output
 config={
-    'pods': ['example'], # list of pods you'd like to run
+    'pods': ['Wheeler_Kiladis'], # list of pods you'd like to run
     'startyr': '1990', # year to start analysis
-    'endyr': '1995' # year to end analysis
+    'endyr': '1995', # year to end analysis
+    'custom_pod_info_json': "",
+    'data_range': [1990, 1995],
+    'use_gfdl_mdtf_env': True
 }
 figures = run_plugin(name, catalog_path, out_dir, config=config)
 print(figures)
